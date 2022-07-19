eb4d34236abc25efd912a3ff5edb35e834eac993
Guilherme Santos <guilherme.santos@cmtecnologia.com.br>
2022-07-19 14:01:14 -0300

    First controller and Razor pages tests

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course.";
+            ViewData["Student"] = "Guilherme Rodrigues";
+            ViewData["Email"] = "guilherme@mail.com";
 
             return View();
         }
