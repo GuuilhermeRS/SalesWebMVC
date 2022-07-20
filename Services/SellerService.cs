b64201667dbf9b3635a86498de25d9bef542a57d
Guilherme Santos <guilherme.gke@gmail.com>
2022-07-20 13:42:06 -0300

    Seller details and eager loading

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Collections.Generic;
 using SalesWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMVC.Services
 {
@@ -26,7 +27,8 @@ namespace SalesWebMVC.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            // Include - eager loading -> carrega outros objetos associados ao objeto principal
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
